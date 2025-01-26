c500f105b9d5cbe35402b195682abc50ffc349cf
varikutinaresh <v.n.rreddy00@gmail.com>
2025-01-26 14:50:18 +0530

    cahnge the region name

--- EKS/provider.tf
+++ EKS/provider.tf
@@ -2,7 +2,7 @@ locals {
   region = "us-east-1"
   name   = "netflix-cluster"
   vpc_cidr = "10.123.0.0/16"
-  azs      = ["us-east-1a", "ap-east-1b"]
+  azs      = ["us-east-1a", "us-east-1b"]
   public_subnets  = ["10.123.1.0/24", "10.123.2.0/24"]
   private_subnets = ["10.123.3.0/24", "10.123.4.0/24"]
   intra_subnets   = ["10.123.5.0/24", "10.123.6.0/24"]
